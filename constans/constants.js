cbdf16f56a6171816def5bcedb8f2d25f7833204
Dima <Slimon1987@gmail.com>
2021-10-08 11:45:35 +0300

    user array

--- constans/constants.js
+++ constans/constants.js
@@ -1,2 +1,3 @@
 const GOOD_PASSWORD = 'qwerty123';
 const MAX_COUNT_TRY = 3;
+const OLD_AGE = 65
--- constans/index.js
+++ constans/index.js
@@ -1,32 +1,44 @@
-// const student = {
-//   id:1,
-//   firsName:'Elon',
-//   lastName:'Mask',
-//   age:50,
-//   isMale:true,
-//   toString: function(){
-//     return '${this.firstName} ${this.lastName}';
-//   }
-// };
-
-// const student2 = {
-//   id:2,
-//   firsName:'Tim',
-//   secondName:'Li',
-//   age:65,
-//   isMale:true,
-// };
-function saySomething (howSay, whatSay){
-  howSay(whatSay);
-}
-saySomething(console.log, 'qwerty');
-
-saySomething(comfirm, 'qwerty');
-
-function isEven(n){
-  return n%2===0;
+function User(firstName, lastName, age, isMale, email, isSubbscriber = false){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.isMale = isMale;
+  this.email = email;
+  this.isSubbscriber = isSubbscriber;
 }
 
-function isOdd(n){
-  return n%2===1;
+function UserPrototype(){
+  this.fullName = function(){
+    return '${this.firstName} ${this.lastName}'
+  }
 }
+
+User.prototype = new UserPrototype();
+
+function createRondomUser(amount = 1){  
+  const db = [];
+  for(let i = 0; i<amount; i++){
+    const user = new User(
+      `Name${i}`,
+      `Lname${i}`,
+      Math.ceil(Math.random()*60+20),
+      Math.random()>0.5,
+      `email${i}@gmail.com`
+    );
+    db.push(user);
+  }
+  return db;
+}
+
+const users = createRondomUser(50);
+console.table(users);
+
+const fullNameUsers = users.map(function(user){
+  return user.fullName();
+});
+
+function isOldPeople(user){
+   return user.age>=OLD_AGE;
+}
+const oldUsers = users.filter(isOldPeople);
+console.table(oldUsers);
